b511bf5a3c135e95cdd4d1329d2da9c9f24e9944
Tiago França <dev@tiagofranca.com>
2023-12-26 16:43:18 -0300

    On branch package/filament 	renamed:    tests/Feature/ExampleTest.php -> tests/Feature/RedirectDashboardTest.php

--- tests/Feature/RedirectDashboardTest.php
+++ tests/Feature/RedirectDashboardTest.php
@@ -5,12 +5,12 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class RedirectDashboardTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function testTheApplicationReturnsASuccessfulResponse(): void
+    public function testRedirectDashboard(): void
     {
         $response = $this->get('/');
 
